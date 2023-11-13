1724d218e998eecee8efe4f1ddf283d490804231
RemiDebruyne <remidebruyne@hotmail.com>
2023-11-13 22:51:33 +0100

    docs : add square-n-sum.js

---
+++ square-n-sum.js
@@ -0,0 +1,13 @@
+/* DESCRIPTION:
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1² + 2² + 2² = 9
+ */
+
+function squareSum(numbers){
+  let result = 0;
+  for (i = 0; i < numbers.length; i++) {
+  result = Math.pow(numbers[i], 2) + result;
+  }
+  return result 
+}

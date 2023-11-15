aa3592865d7cf8af151bb79365d723e41eb9d08a
RemiDebruyne <remidebruyne@hotmail.com>
2023-11-15 16:10:01 +0100

    docs : add beginner-reudce-but-grow.js

---
+++ beginner-reduce-but-grow.js
@@ -0,0 +1,9 @@
+/* Description
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24 */
+
+function grow(arr) {
+    return arr.reduce((current, sum) => current * sum);
+}

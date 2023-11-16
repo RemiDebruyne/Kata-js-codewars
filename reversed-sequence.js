11160d949ccf85ab0b0e4d12d39a6dc5b36ab30f
RemiDebruyne <remidebruyne@hotmail.com>
2023-11-16 12:37:07 +0100

    docs: add reversed-sequence.js

---
+++ reversed-sequence.js
@@ -0,0 +1,12 @@
+/* Description 
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]*/
+
+const reverseSeq = n => {
+  let result = []
+  for (n += 1; n > 1; n--){
+    result.push(n - 1);
+  }
+    return result;
+};

bde1eed768c4587c882775a0bae105c01bdd3b8c
RemiDebruyne <remidebruyne@hotmail.com>
2023-11-16 23:21:01 +0100

    docs: add total-amount-of-points.js

---
+++ total-amount-of-points.js
@@ -0,0 +1,36 @@
+/* Description
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+Notes:
+
+our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4 */
+
+function points(games) {
+    let teamA = games.map((x) => Number(x[0]))
+    let teamB = games.map((x) => Number(x[2]))
+    let result = 0;
+    for (let i = 0; i < teamA.length; i++) {
+        if (teamA[i] < teamB[i]) {
+            continue;
+        } else if (teamA[i] > teamB[i]) {
+            result += 3;
+        } else {
+            result += 1;
+        }
+        console.log(result);
+    }
+    return result;
+}
